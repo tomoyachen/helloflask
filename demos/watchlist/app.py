2d45b100415e12d58128600957de825a09a79e0e
tomoya_chen <tomoya_chen@163.com>
2019-10-15 00:41:13 +0800

    Day 7 v2

--- demos/watchlist/app.py
+++ demos/watchlist/app.py
@@ -1,8 +1,21 @@
 from flask import Flask, url_for, render_template
 from faker import Faker
+from flask_sqlalchemy import SQLAlchemy
+import os, sys, click
+
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
 
 
 app = Flask(__name__)
+fake = Faker('zh_CN')
+db = SQLAlchemy(app) # 初始化扩展，传入程序实例 app
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 
 
 @app.route('/home')
@@ -47,9 +60,9 @@ movies = [
 
 @app.route("/more")
 def more():
-    fake = Faker('zh_CN')
+
     movies_list_tmp = []
-    for i in range(0,10):
+    for i in range(0,1):
         moves_info_tmp = {}
         moves_info_tmp["title"] = fake.name()
         moves_info_tmp["year"] = fake.year()
@@ -57,6 +70,24 @@ def more():
     movies.extend(movies_list_tmp)
     return "0x000000"
 
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 
 if __name__ == "__main__":
     app.run()
