b62a4b4654e34e977e45616feb2027b1960e5897
tomoya_chen <tomoya_chen@163.com>
2019-10-15 00:19:07 +0800

    Day 7

--- demos/watchlist/app.py
+++ demos/watchlist/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
+from faker import Faker
+
 
 app = Flask(__name__)
 
-@app.route('/')
+
 @app.route('/home')
-@app.route('/index')
-def hello():
+def home():
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
 
@@ -24,6 +25,37 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html", name = name, movies = movies)
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+@app.route("/more")
+def more():
+    fake = Faker('zh_CN')
+    movies_list_tmp = []
+    for i in range(0,10):
+        moves_info_tmp = {}
+        moves_info_tmp["title"] = fake.name()
+        moves_info_tmp["year"] = fake.year()
+        movies_list_tmp.append(moves_info_tmp)
+    movies.extend(movies_list_tmp)
+    return "0x000000"
 
 
 if __name__ == "__main__":
