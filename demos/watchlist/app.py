21f6f71f39b21dcc3569297ce44be7cd70e01efd
tomoya_chen <tomoya_chen@163.com>
2019-10-15 20:07:39 +0800

    Day 7 v2

--- demos/watchlist/app.py
+++ demos/watchlist/app.py
@@ -42,32 +42,23 @@ def test_url_for():
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", name = name, movies = movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template("index.html", user = user.name, movies = movies)
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
 
-@app.route("/more")
-def more():
+@app.route("/addMovie")
+def addMovie():
+    movie_new = Movie(title=fake.name(), year=fake.year())
+    db.session.add(movie_new)
+    db.session.commit()
+    return "0x000000"
 
-    movies_list_tmp = []
-    for i in range(0,1):
-        moves_info_tmp = {}
-        moves_info_tmp["title"] = fake.name()
-        moves_info_tmp["year"] = fake.year()
-        movies_list_tmp.append(moves_info_tmp)
-    movies.extend(movies_list_tmp)
+@app.route("/deleteMovie")
+def deleteMovie():
+    movie = Movie.query.order_by(-Movie.id).first()
+    db.session.delete(movie)
+    db.session.commit()
     return "0x000000"
 
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
@@ -79,6 +70,7 @@ class Movie(db.Model):  # 表名将会是 movie
     id = db.Column(db.Integer, primary_key=True)  # 主键
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
+
 
 @app.cli.command()  # 注册为命令
 @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
