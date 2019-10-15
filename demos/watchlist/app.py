6340253990c829b5bbc6c4576d6581d5fe473ed2
tomoya_chen <tomoya_chen@163.com>
2019-10-15 12:36:26 +0800

    Day 8

--- demos/watchlist/app.py
+++ demos/watchlist/app.py
@@ -4,19 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 import os, sys, click
 
 
+app = Flask(__name__)
+
+fake = Faker('zh_CN')
+
 WIN = sys.platform.startswith('win')
 if WIN:  # 如果是 Windows 系统，使用三个斜线
     prefix = 'sqlite:///'
 else:  # 否则使用四个斜线
     prefix = 'sqlite:////'
 
-
-app = Flask(__name__)
-fake = Faker('zh_CN')
-db = SQLAlchemy(app) # 初始化扩展，传入程序实例 app
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
-
+db = SQLAlchemy(app) # 初始化扩展，传入程序实例 app
 
 @app.route('/home')
 def home():
