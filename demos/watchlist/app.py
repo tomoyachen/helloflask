3809321bd51c09f8b81d11a3e894385280cb874c
tomoya_chen <tomoya_chen@163.com>
2019-10-18 14:58:01 +0800

    Day 10

--- demos/watchlist/app.py
+++ demos/watchlist/app.py
@@ -199,7 +199,7 @@ class Movie(db.Model):  # 表名将会是 movie
 
 
 @app.cli.command()  # 注册为命令
-@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项 #is_flag不用输入参数
 def initdb(drop):
     """Initialize the database."""
     if drop:  # 判断是否输入了选项
